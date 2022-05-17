e882d1c2b76d388a911aa0ee1e347ee156a87137
ThiagoEliasSSP <thiagoe.ssp@gmail.com>
2022-05-16 23:26:52 -0300

    Database incompleto

---
+++ src/database/schema.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE Plusle;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS cursos (
+  id_curso UUID NOT NULL UNIQUE DEFAULT uui_generate_v4(),
+  nome VARCHAR NOT NULL,
+  maximo_horas INT
+);
+
+CREATE TABLE IF NOT EXISTS alunos (
+  id_aluno UUID NOT NULL UNIQUE DEFAULT uui_generate_v4(),
+  email VARCHAR NOT NULL UNIQUE,
+  nome VARCHAR NOT NULL,
+  celular VARCHAR NOT NULL UNIQUE,
+  id_curso UUID,
+  horas_totais INT,
+  horas_acumuladas INT,
+  FOREIGN KEY(id_curso) REFERENCES cursos(id)
+);
+
+CREATE TABLE IF NOT EXISTS professores (
+  id_professor UUID NOT NULL UNIQUE DEFAULT uui_generate_v4(),
+  email VARCHAR NOT NULL UNIQUE,
+  nome VARCHAR NOT NULL,
+  celular VARCHAR NOT NULL UNIQUE,
+);
+
+CREATE TABLE IF NOT EXISTS categoria_atividades (
+  id_categoria UUID NOT NULL UNIQUE DEFAULT uui_generate_v4(),
+  nome VARCHAR NOT NULL,
+  maximo_horas INT,
+);
+
+CREATE TABLE IF NOT EXISTS atividades (
+  id_atividade UUID NOT NULL UNIQUE DEFAULT uui_generate_v4(),
+  arquivo VARCHAR NOT NULL,
+  nome VARCHAR NOT NULL,
+  horas VARCHAR NOT NULL,
+  status VARCHAR NOT NULL,
+  FOREIGN KEY(id_aluno) REFERENCES alunos(id_aluno)
+  FOREIGN KEY(id_professor) REFERENCES professores(id_professor)
+  FOREIGN KEY(id_categoria) REFERENCES categoria_atividades(id_categoria)
+);
